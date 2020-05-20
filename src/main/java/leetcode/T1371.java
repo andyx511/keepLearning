afa2650873dc6ad6f558d04d4c06922c8f1768de
AlexNi <1812425414@qq.com>
2020-05-20 15:39:11 +0800

    增加自定义注解部分

---
+++ src/main/java/leetcode/T1371.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * @Classname T1371
+ * @Description TODO
+ * @Date 2020/5/20 13:39
+ * @Author ALexNi
+ */
+public class T1371 {
+    public static void main(String[] args) {
+
+    }
+    public static void findTheLongestSubstring(String s) {
+        for (int i = 0; i < s.length(); i++){
+
+        }
+    }
+}
