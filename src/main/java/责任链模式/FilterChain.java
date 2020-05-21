dd1ca2170fe236c05c764536c1faa97a82ecd6ab
AlexNi <1812425414@qq.com>
2020-05-21 15:59:49 +0800

    新增责任链模块

---
+++ src/main/java/责任链模式/FilterChain.java
@@ -0,0 +1,30 @@
+package 责任链模式;
+
+import 责任链模式.FilterImpl.AppleFilter;
+import 责任链模式.FilterImpl.BananaFilter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @Classname FilterChain
+ * @Description TODO
+ * @Date 2020/5/21 15:47
+ * @Author ALexNi
+ */
+public class FilterChain {
+
+    List<Filter> filters = new ArrayList<>();
+
+    public FilterChain(){
+        filters.add(new AppleFilter());
+        filters.add(new BananaFilter());
+    }
+
+    public void processData(Set<String> strings){
+        for (Filter filter : filters){
+            filter.doFilter(strings);
+        }
+    }
+}
