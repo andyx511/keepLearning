5a696378d1afc171bbd6de076bb92c67dee427b3
AlexNi <1812425414@qq.com>
2020-05-21 10:18:29 +0800

    增加leetcode第 5题

---
+++ src/main/java/leetcode/T5最长回文串.java
@@ -0,0 +1,38 @@
+package leetcode;
+
+/**
+ * @Classname T5最长回文串
+ * @Description TODO
+ * @Date 2020/5/21 9:29
+ * @Author ALexNi
+ */
+public class T5最长回文串 {
+    public static void main(String[] args) {
+        System.out.println(longestPalindrome("bb"));
+    }
+    public static String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) {
+            return "";
+        }
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private static int  expandAroundCenter(String s, int left, int right) {
+        int L = left, R = right;
+        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+            L--;
+            R++;
+        }
+        return R - L - 1;
+    }
+}
