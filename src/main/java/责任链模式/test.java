45008544cbbca6b5d8d96278765c5ff2feca2a1f
AlexNi <1812425414@qq.com>
2020-05-21 16:00:54 +0800

    测试文件

---
+++ src/main/java/责任链模式/test.java
@@ -0,0 +1,22 @@
+package 责任链模式;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @Classname test
+ * @Description TODO
+ * @Date 2020/5/21 15:46
+ * @Author ALexNi
+ */
+public class test {
+    public static void main(String[] args) {
+        Set<String> strings = new HashSet<>();;
+        strings.add("apple");
+        strings.add("banana");
+        strings.add("org");
+        FilterChain chain = new FilterChain();
+        chain.processData(strings);
+        System.out.println(strings.toString());
+    }
+}
