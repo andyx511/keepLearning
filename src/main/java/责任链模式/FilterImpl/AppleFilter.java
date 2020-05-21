d28e9c78c2acbf8b16aeb84c2a65fc2cc5e0e4d9
AlexNi <1812425414@qq.com>
2020-05-21 16:01:21 +0800

    增加AppleFilter

---
+++ src/main/java/责任链模式/FilterImpl/AppleFilter.java
@@ -0,0 +1,21 @@
+package 责任链模式.FilterImpl;
+
+import 责任链模式.Filter;
+
+import java.util.Set;
+
+/**
+ * @Classname AppleFilter
+ * @Description TODO
+ * @Date 2020/5/21 15:45
+ * @Author ALexNi
+ */
+public class AppleFilter implements Filter {
+
+    @Override
+    public void doFilter(Set<String> strings) {
+        if (strings.contains("apple")){
+            strings.remove("apple");
+        }
+    }
+}
