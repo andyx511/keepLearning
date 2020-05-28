275cfc2eb3012defd1ae83a9f1a13e7a83f1b84b
1812425414 <1812425414@qq.com>
2020-05-28 17:18:49 +0800

    算法怎么就这么难呢？

---
+++ src/main/java/leetcode/T394字符串解析.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @Classname T394字符串解析
+ * @Description TODO
+ * s = "3[a]2[bc]", 返回 "aaabcbc".
+ * s = "3[a2[c]]", 返回 "accaccacc".
+ * s = "2[abc]3[cd]ef", 返回 "abcabccdcdcdef".
+ * @Date 2020/5/28 10:23
+ * @Author ALexNi
+ */
+public class T394字符串解析 {
+    public static void main(String[] args) {
+        T394字符串解析 a = new T394字符串解析();
+        String s = "11[er]qw";
+        System.out.println(a.decodeString(s));
+
+    }
+
+    public String decodeString(String s) {
+        return null;
+    }
+}
